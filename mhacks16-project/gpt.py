f27f2389936d8906142e694c9a8167f9accea3df
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2023-11-18 14:05:03 -0500

    Move API key to .env file

--- mhacks16-project/gpt.py
+++ mhacks16-project/gpt.py
@@ -1,15 +1,29 @@
+import os
+
+from dotenv import load_dotenv
 from openai import AzureOpenAI
 
-client = AzureOpenAI(api_key='API_KEY',
-azure_endpoint='https://api.umgpt.umich.edu/azure-openai-api/ptu',
-api_version='2023-03-15-preview')
+load_dotenv()
+API_KEY = os.environ["API_KEY"]
+
+client = AzureOpenAI(
+    api_key=API_KEY,
+    azure_endpoint="https://api.umgpt.umich.edu/azure-openai-api/ptu",
+    api_version="2023-03-15-preview",
+)
 
 completion = client.chat.completions.create(
-  model="gpt-4",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
+    model="gpt-4",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+        },
+        {
+            "role": "user",
+            "content": "Compose a poem that explains the concept of recursion in programming.",
+        },
+    ],
 )
 
 print(completion.choices[0].message.content)
