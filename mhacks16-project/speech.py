a44beb1ec1956ac546395bcb954a235b2c85eb87
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2023-11-18 14:47:35 -0500

    Add resources path to speech recognition

--- mhacks16-project/speech.py
+++ mhacks16-project/speech.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 import speech_recognition as sr
 
+
+PROJECT_ROOT = Path(__file__).parent.parent.resolve()
+RESOURCES_ROOT = PROJECT_ROOT / "resources"
+
+harvard_path = str(RESOURCES_ROOT / "harvard.wav")
 r = sr.Recognizer()
-harvard = sr.AudioFile('harvard.wav')
+harvard = sr.AudioFile(harvard_path)
 with harvard as source:
     audio = r.record(source)
 
