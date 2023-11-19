eadbc613ccf180e20be8e74fdae87ec62a7c853a
Ruthesh-Thavamani <truthesh@gmail.com>
2023-11-18 23:55:18 -0500

    Finished coding main

--- mhacks16-project/emotion.py
+++ mhacks16-project/emotion.py
@@ -3,50 +3,64 @@ from deepface import DeepFace
 import numpy as np
 import os
 
-VIDEOS_DIR = os.path.join('.', 'mhacks16-project/videos')
-VIDEO_NAME = 'interview2.mp4'
-emotion_color_mapping = {
-    'angry': (0, 0, 255),
-    'disgust': (0, 0, 255),
-    'fear': (0, 0, 255),
-    'sad': (0, 0, 255),
-    'happy': (0, 255, 0),
-    'surprise': (0, 255, 0),
-    'neutral': (128, 128, 128),
-}
+emotions = {}
 
-video_path = os.path.join(VIDEOS_DIR, VIDEO_NAME)
-video_path_out = '{}_out.mp4'.format(video_path[:-4])
-print(video_path)
-cap = cv2.VideoCapture(video_path)
-ret, frame = cap.read()
-H, W, _ = frame.shape
-out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'mp4v'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+def emotion_detection(filename):
+    VIDEOS_DIR = os.path.join('.', 'mhacks16-project/videos')
+    VIDEO_NAME = filename
+    emotion_color_mapping = {
+        'angry': (0, 0, 255),
+        'disgust': (0, 0, 255),
+        'fear': (0, 0, 255),
+        'sad': (0, 0, 255),
+        'happy': (0, 255, 0),
+        'surprise': (0, 255, 0),
+        'neutral': (128, 128, 128),
+    }
 
-face_cascade = cv2.CascadeClassifier("mhacks16-project/haarcascade_frontalface_default.xml")
-
-
-while ret:
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face=face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
-    for x,y,w,h in face:
-        if(w > 100):
-            try:
-                analyze = DeepFace.analyze(frame,actions=['emotion'],silent = True)
-                emotion = analyze[0]['dominant_emotion']
-                color = emotion_color_mapping.get(emotion)
-                img = cv2.rectangle(frame,(x,y),(x+w, y+w),color,4)
-                cv2.putText(frame, emotion.upper(), (int(x), int(y - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, color, 3, cv2.LINE_AA)
-                print(emotion)
-            except:
-                print("no face")
-        else:
-            print("no face")
-    out.write(frame)
+    video_path = os.path.join(VIDEOS_DIR, VIDEO_NAME)
+    video_path_out = '{}_out.mp4'.format(video_path[:-4])
+    print(video_path)
+    cap = cv2.VideoCapture(video_path)
     ret, frame = cap.read()
+    H, W, _ = frame.shape
+    out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'mp4v'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    face_cascade = cv2.CascadeClassifier("mhacks16-project/haarcascade_frontalface_default.xml")
+
+
+    while ret:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face=face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+        for x,y,w,h in face:
+            if(w > 100):
+                try:
+                    analyze = DeepFace.analyze(frame,actions=['emotion'],silent = True)
+                    emotion = analyze[0]['dominant_emotion']
+                    color = emotion_color_mapping.get(emotion)
+                    img = cv2.rectangle(frame,(x,y),(x+w, y+w),color,4)
+                    cv2.putText(frame, emotion.upper(), (int(x), int(y - 10)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.3, color, 3, cv2.LINE_AA)
+                    if emotion in emotions:
+                        emotions[emotion] += 1
+                    else:
+                        emotions[emotion] = 1
+                except:
+                    if "no face" in emotions:
+                        emotions["no face"] += 1
+                    else:
+                        emotions["no face"] = 1
+            else:
+                if "no face" in emotions:
+                    emotions["no face"] += 1
+                else:
+                    emotions["no face"] = 1
+        out.write(frame)
+        ret, frame = cap.read()
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+    return emotions
--- mhacks16-project/main.py
+++ mhacks16-project/main.py
@@ -1,11 +1,25 @@
 import speech_to_text
 import gpt
+import emotion
+from moviepy.editor import VideoFileClip
 
 def main():
-    transcript = speech_to_text.speech_to_text("harvard.wav")
-    question = ""
+    question = input("Enter an interview question: ")
+    video_filename = input("Enter a video of you answering that question: ")
+
+    video = VideoFileClip(video_filename)
+    audio = video.audio
+    audio.write_audiofile('audio.wav')
+
+    transcript = speech_to_text.speech_to_text("/Users/truth/Mhacks16-Project/audio.wav")
     gpt_analysis = gpt.gpt_response(question, transcript)
-    print (transcript)
+    print(gpt_analysis)
+
+    emotion_dict = emotion.emotion_detection(video_filename)
+
+    for i in emotion_dict:
+        print("You experienced " + i + " " + str(emotion_dict[i]) + "  times")
+
     
 
 if(__name__ == "__main__"):
