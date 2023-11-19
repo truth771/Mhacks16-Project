b050cb9ea586c0dfd780d8a849739c2a99a78a2e
Ruthesh-Thavamani <truthesh@gmail.com>
2023-11-19 02:56:53 -0500

    Added GPT output for emotions

--- mhacks16-project/main.py
+++ mhacks16-project/main.py
@@ -1,24 +1,28 @@
 import speech_to_text
 import gpt
 import emotion
+import logging
 from moviepy.editor import VideoFileClip
+
+logging.getLogger('moviepy').setLevel(logging.CRITICAL)
 
 def main():
     question = input("Enter an interview question: ")
     video_filename = input("Enter a video of you answering that question: ")
+    print()
 
     video = VideoFileClip(video_filename)
     audio = video.audio
-    audio.write_audiofile('audio.wav')
+    audio.write_audiofile('audio.wav',logger=None)
 
     transcript = speech_to_text.speech_to_text("/Users/truth/Mhacks16-Project/audio.wav")
+
     gpt_analysis = gpt.gpt_response(question, transcript)
     print(gpt_analysis)
+    print("\n")
 
     emotion_dict = emotion.emotion_detection(video_filename)
-
-    for i in emotion_dict:
-        print("You experienced " + i + " " + str(emotion_dict[i]) + " times")
+    print(gpt.gpt_emotions(emotion_dict))
 
     
 
