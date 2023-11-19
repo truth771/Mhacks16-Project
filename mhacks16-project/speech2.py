c9f120031da98dabc35a652f6b564b46cd76a57b
Ruthesh-Thavamani <truthesh@gmail.com>
2023-11-18 19:53:50 -0500

    Add STT file

---
+++ mhacks16-project/speech2.py
@@ -0,0 +1,26 @@
+from google.cloud import speech
+from pathlib import Path
+
+def run_quickstart() -> speech.RecognizeResponse:
+    # Instantiates a client
+    client = speech.SpeechClient()
+
+    # The name of the audio file to transcribe
+    gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code="en-US",
+    )
+
+    # Detects speech in the audio file
+    response = client.recognize(config=config, audio=audio)
+
+    for result in response.results:
+        print(f"Transcript: {result.alternatives[0].transcript}")
+
+if __name__ == "__main__":
+    run_quickstart()
